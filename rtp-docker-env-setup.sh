0d9fce06d93464da9a882182d22069b62f82bb39
yohay <yohayg@gmail.com>
2017-12-28 15:44:19 +0200

    rtp-docker-dev

---
+++ rtp-docker-env-setup.sh
@@ -0,0 +1,206 @@
+#!/bin/zsh
+
+
+
+if [[ ! -d $1 ]]; then
+    echo "Error: $1 is not a valid directory"
+    echo "Please provide the path to your your git directory"
+    exit 1
+fi
+echo "Using git directory: $1"
+export GIT_PATH=${1%/}
+export RTP_DOCKER_HOME=$GIT_PATH/JenkinsScripts/docker-dev
+
+function add_rtp_zsh {
+    echo "Creating rtp zsh plugin at /.oh-my-zsh/plugins/rtp/"
+    
+    [ -d ~/.oh-my-zsh/plugins/rtp ] || mkdir ~/.oh-my-zsh/plugins/rtp
+
+    
+    cat > ~/.oh-my-zsh/plugins/rtp/rtp.plugin.zsh << EOF
+        
+        export GIT_PATH=$GIT_PATH
+        export RTP_DOCKER_HOME=$GIT_PATH/JenkinsScripts/docker-dev
+
+        alias mysql="docker-compose -f $RTP_DOCKER_HOME/docker-compose.yaml exec mysql.docker mysql -usa -psa"
+        alias redis-cli="docker-compose -f $RTP_DOCKER_HOME/docker-compose.yaml exec redis.master.docker redis-cli"
+        alias mongo="docker-compose -f $RTP_DOCKER_HOME/docker-compose.yaml exec mongo.docker mongo"
+     
+
+        function bo_set_ver {
+            echo "Setting BO version"
+            bo_ver=\$(command grep --max-count=1 '<version>' $GIT_PATH/RtpBackOffice/pom.xml | awk -F '>' '{ print \$2 }' | awk -F '<' '{ print \$1 }')
+            echo "BO_VER=\$bo_ver"
+            echo "BO_VER=\$bo_ver" > ${RTP_DOCKER_HOME}/.env
+        }
+        function exit_on_fail {
+            echo "Running mvn clean install \$2 -Dmaven.test.skip=true -f $GIT_PATH/\$1/pom.xml"
+            mvn clean install \$2 -Dmaven.test.skip=true -f $GIT_PATH/\$1/pom.xml
+            rc=\$?
+            if [[ \$rc -ne 0 ]] ; then
+              echo "could not install \$1"
+              return 0
+            fi
+            return 1
+        }
+     
+        function rtp-up {
+            cd $RTP_DOCKER_HOME
+            bo_set_ver
+            exit_on_fail RtpDX || exit_on_fail RtpCEP || exit_on_fail RtpBackOffice || docker-compose up -d
+        }
+     
+        function rtp-down {
+            cd $RTP_DOCKER_HOME
+            docker-compose down
+        }
+ 
+        function rtp-start {
+            cd $RTP_DOCKER_HOME
+            bo_set_ver
+            exit_on_fail RtpDX || exit_on_fail RtpCEP || exit_on_fail RtpBackOffice || docker-compose start
+        }
+ 
+        function rtp-stop {
+            cd $RTP_DOCKER_HOME
+            docker-compose stop
+        }
+ 
+        function bo-restart {
+            cd $RTP_DOCKER_HOME
+            docker-compose stop bo.docker
+            exit_on_fail RtpCore -o || exit_on_fail RtpCommon -o || exit_on_fail RtpTRWCommons -o || exit_on_fail RtpBackOffice -o || mvn  war:exploded -q -o -Dmaven.test.skip=true -f $GIT_PATH/RtpBackOffice/pom.xml || docker-compose start bo.docker
+        }
+
+        function cep-restart {
+            cd $RTP_DOCKER_HOME
+            docker-compose stop cep.docker
+            exit_on_fail RtpCore -o || exit_on_fail RtpCommon -o || exit_on_fail RtpTRWCommons -o || exit_on_fail RtpCEP -o || docker-compose start cep.docker     
+        }
+EOF
+
+    echo "oh-my-zsh/ rtp plugin created at /.oh-my-zsh/plugins/rtp/"
+}
+
+
+if [[ $(command -v brew) == "" ]]; then
+    echo "Installing Hombrew"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    echo "Updating Homebrew"
+    brew update
+fi
+
+if [[ $(command -v docker) == "" ]]; then
+    echo "Installing Docker"
+    brew cask install docker
+    echo "Starting docker"
+    open /Applications/Docker.app
+else
+    echo "Docker already installed"
+    open /Applications/Docker.app
+fi
+
+if [[ $(command brew list | grep dnsmasq) == "" ]]; then
+    echo "Installing dnsmasq"
+    brew install dnsmasq
+    echo "Configuring"
+    mkdir -pv $(brew --prefix)/etc/
+    echo 'address=/.docker/127.0.0.1' > $(brew --prefix)/etc/dnsmasq.conf
+    sudo cp -v $(brew --prefix dnsmasq)/homebrew.mxcl.dnsmasq.plist /Library/LaunchDaemons
+    sudo mkdir -v /etc/resolver
+    sudo bash -c 'echo "nameserver 127.0.0.1" > /etc/resolver/docker'
+    echo "Luanching damon"
+    sudo launchctl load -w /Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist
+else
+    echo "dnsmask already installed"
+fi
+
+if [[ $(command -v mvn) == "" ]]; then
+    echo "Installing Maven"
+    brew install maven
+else
+    echo "Maven already installed"
+fi
+
+if [[ ! -d $GIT_PATH/JenkinsScripts ]]; then
+    echo "Cloning JenkinsScripts to $GIT_PATH/JenkinsScripts"
+    git clone git@gitlab.marketo.org:RTP/JenkinsScripts.git
+    chmod +x JenkinsScripts/docker-dev/cloneRepos.sh
+    cd $GIT_PATH ; ./JenkinsScripts/docker-dev/cloneRepos.sh
+else
+    echo "JenkinsScripts already exists in $GIT_PATH/JenkinsScripts"
+fi
+
+
+if [[ $(command ssh-add -l | grep  `ssh-keygen -lf ~/.ssh/id_rsa  | awk '{print $2}'`) == "" ]]; then
+    echo "Adding key to  ~/.ssh/id_rsa"
+    ssh-add -K ~/.ssh/id_rsa
+else
+    echo "Key ~/.ssh/id_rsa already exists"
+fi
+
+
+if [[ ! -d ~/.oh-my-zsh ]]; then
+    echo "Installing oh my zsh. This will abort your script. Please re-run the script again"
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else
+    echo "oh-my-zsh already exists"
+fi
+
+add_rtp_zsh
+echo "Checking zsh plugins at .zshrc"
+if [[ $(command cat ~/.zshrc | grep "^plugins=($") == "" ]]; then
+    echo "zsh plugins already installed at .zshrc"
+else
+    echo "Installing zsh plugins at .zshrc"
+    ex -sc '%s/plugins=(\n  git\n)/plugins=(git docker kubectl docker-compose mvn iterm2 sublime rtp)/g|x' ~/.zshrc
+    echo "Zsh plugins installed at .zshrc"
+
+    
+fi
+echo ""
+echo ""
+echo "  ____ _____ ____    ____               ____        _            ";
+echo " |  _ \_   _|  _ \  |  _ \  _____   __ |  _ \ _   _| | ___  ___  ";
+echo " | |_) || | | |_) | | | | |/ _ \ \ / / | |_) | | | | |/ _ \/ __| ";
+echo " |  _ < | | |  __/  | |_| |  __/\ V /  |  _ <| |_| | |  __/\__ \ ";
+echo " |_| \_\|_| |_|     |____/ \___| \_/   |_| \_\\__,_|_|\___||___/ ";
+echo "                                                                 ";    
+echo ""
+echo "You are all set"
+echo "You can now use the following commands: rtp-up rtp-down rtp-start rtp-stop bo-restart cep-restart"
+echo "Enjoy!"
+
+
+
+echo "-----------------------------------------------------------"
+echo "                  Few notes!"
+echo "1. Switch to zsh by typing: zsh"
+echo "2. Please try to autocomplete rtp in command line"
+echo "   If it doesn't work please type source ~/.zshrc"
+echo "3. Make sure docker is running before running rtp-up"
+echo "-----------------------------------------------------------"
+
+echo "Your docker configuration:"
+memory=$(command grep "  \"memoryMiB\" : [0-9]*," ~/Library/Group\ Containers/group.com.docker/settings.json | awk '{print $3}' | rev | cut -c 2- | rev)
+cpu=$(command grep "  \"cpus\" : [0-9]*," ~/Library/Group\ Containers/group.com.docker/settings.json | awk '{print $3}' | rev | cut -c 2- | rev)
+
+echo "Memory: $memory"
+echo "CPUs: $cpu"
+
+if [[ $cpu < 5 ]]; then
+    echo "Please add more CPUs to Docker daemon. You need at least 4 CPUs"
+    echo "Set Docker computing resources: Docker menu > Preferences > Advanced > Set cpu = 4"
+fi
+
+if [[ $memory < 7000 ]]; then
+    echo "Please add more memory to Docker daemon. You need at least 6 6GB"
+    echo "Set Docker computing resources: Docker menu > Preferences > Advanced > Set memory = 6GB"
+fi
+
+echo "Attempting to switch to zsh."
+zsh
+echo "Running source ~/.zshrc"
+source ~/.zshrc
+echo "Running source ~/.zshrc is done"
